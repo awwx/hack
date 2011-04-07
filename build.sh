29eb09133b5bd37fc8c632fcc3dc93b31e166ea7
Andrew Wilcox <andrew.wilcox@gmail.com>
2011-04-07 14:24:48 -0700

    update build.sh to automatically push changes to hackbin

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e -v
 
+revision=`git log -n 1 --format=format:%H`
+echo revision $revision
 /code/hackbin/hack --apply --destdir /tmp/hackbin --clean hackinator.recipe
 cp kb.recipe /tmp/hackbin/
+
+cd /code/hackbin
+cp /tmp/hackbin/* .
+git commit -a -m "built from https://github.com/awwx/hack commit $revision"
